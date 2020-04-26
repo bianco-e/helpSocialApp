b702250d4a425a1b0808bb12afad0f01bf6d8902
Bianco <bianco.emiliozzi@gmail.com>
2020-04-26 00:36:06 -0300

    :tada:

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import firebase from "../data/firebase";
 
 const Context = React.createContext({});
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
+
+  useEffect(() => {
+    firebase.auth.onAuthStateChanged((user) => {
+      if (user) setUser(user);
+      else setUser(undefined);
+    });
+  });
 
   return (
     <Context.Provider value={{ user, setUser }}>{children}</Context.Provider>
